e91349c249707f7826b2f1129169e38e200559d5
GLD5000 <garethlouisdevlin@gmail.com>
2022-10-13 18:01:59 +0100

    Added login

--- public/app.js
+++ public/app.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCEm6dnpThXzgVIUBg_phBPZtyb1l9y9hM",
   authDomain: "first-project-8b0b2.firebaseapp.com",
@@ -13,11 +5,39 @@ const firebaseConfig = {
   storageBucket: "first-project-8b0b2.appspot.com",
   messagingSenderId: "955240141201",
   appId: "1:955240141201:web:976a37b4542930a5300a30",
-  measurementId: "G-GG8XMFCT2T"
+  measurementId: "G-GG8XMFCT2T",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
 
 console.log(firebase);
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const elements = {};
+elements.array = [
+  "signInBtn",
+  "signOutBtn",
+  "whenSignedIn",
+  "whenSignedOut",
+  "userDetails",
+];
+
+elements.array.forEach((elementName) => {
+  elements[elementName] = document.getElementById(elementName);
+});
+
+elements.signInBtn.onclick = () => auth.signInWithPopup(provider);
+elements.signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    elements.whenSignedIn.hidden = false;
+    elements.whenSignedOut.hidden = true;
+    elements.userDetails.innerHTML = `Welcome ${user.displayName}`;
+  } else {
+    elements.whenSignedIn.hidden = true;
+    elements.whenSignedOut.hidden = false;
+  }
+});
+
+console.log(elements);
